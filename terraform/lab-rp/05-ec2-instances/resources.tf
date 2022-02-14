23b7d4b540ffd98526926d554fc90bf0d45f9bb1
Rocio Philipps <yonepv@hotmail.com>
2022-02-14 10:44:36 -0500

    S5-5-2 EC2 instance with html file

--- terraform/lab-rp/05-ec2-instances/resources.tf
+++ terraform/lab-rp/05-ec2-instances/resources.tf
@@ -26,3 +26,27 @@ resource "aws_security_group" "http_server_sg" {
     name   = "http_server_sg"
   }
 }
+
+resource "aws_instance" "http_server" {
+  ami = ""
+  key_name = "carredesable1-rp"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id = ""
+
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = ""
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",//install http
+      "sudo service httpd start",//start
+      "echo Welcome to in28minutes - Virtual Server is at ${self.public_dns} | sudo tee /var/wwww/html/index.html"//copy a file
+    ]
+    
+  }
+}
--- terraform/lab-rp/05-ec2-instances/variables.tf
+++ terraform/lab-rp/05-ec2-instances/variables.tf
@@ -10,4 +10,8 @@ variable "aws_vpc_selected_dev" {
   description = "VPC selected is dev"
   default = "vpc-012d34854369f0a06"//dev1 vpc id//"vpc-089ee9edc5b376a32"
 }
+
+variable "aws_key_pair" {
+  default = "~/.aws/aws_keys/carredesable1-rp.pem"  
+}
 # ===================================================
